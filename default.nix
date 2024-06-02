e7e2982396740d51bf4ab4231680a1a7a9089d3f
Cassie Cheung <me@soopy.moe>
2024-06-02 23:52:13 +0800

    feat: add nix config

---
+++ default.nix
@@ -0,0 +1,28 @@
+with import <nixpkgs> {}; mkShellNoCC {
+    packages = [
+        python3Packages.transformers
+        python3Packages.pytorch
+        python3Packages.requests
+        python3Packages.accelerate
+        (python3Packages.buildPythonPackage rec {
+            pname = "quanto";
+            version = "0.2.0";
+            src = python3Packages.fetchPypi {
+                inherit pname version;
+                hash = "sha256-HVWdudXQ86RUj6EaB9WrqHXD3cl+xoWRrZrsXMAj4Qw=";
+            };
+            pyproject = true;
+            build-system = with python3Packages; [
+                setuptools
+                setuptools-scm
+            ];
+
+            dependencies = with python3Packages; [
+                pytorch
+                ninja
+                numpy
+                safetensors
+            ];
+        })
+    ];
+}
